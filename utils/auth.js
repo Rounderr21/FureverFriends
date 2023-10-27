3b9453209ff85550b6564ad94a39df6d0425663d
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-26 20:00:28 -0700

    inital commit of files

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+//example would be
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
