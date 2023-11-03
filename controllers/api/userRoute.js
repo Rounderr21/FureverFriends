f95f24b23b50be36256e8a96c472b999b47c303d
viaheather <viaheatherr@gmail.com>
2023-11-02 21:08:42 -0700

    temporary get route for users

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -4,8 +4,18 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 
+// TEMP GET ROUTE
+router.get("/", async (req, res) => {
+  try {
+    const dbUserData = await User.findAll();
+    res.status(200).json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
@@ -44,7 +54,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -53,6 +63,5 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
-
 
 module.exports = router;
