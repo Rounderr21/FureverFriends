50655386fd46aa0dc2d1096a7b5eea2fac24378d
viaheather <viaheatherr@gmail.com>
2023-11-04 21:34:35 -0700

    deleted a wish route i forgot about

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const userRoute = require("./userRoute");
 const petRoute = require("./petRoute");
-const wishRoute = require("./wishRoute");
 
 router.use("/user", userRoute);
 router.use("/pet", petRoute);
-router.use("/wishlist", wishRoute);
 
 module.exports = router;
