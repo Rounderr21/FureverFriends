c6286c840f16a2aa327d7ef83a1206fe1f77c877
Cody Schwengler01~ <cschwengler@outlook.com>
2023-11-05 17:34:49 -0800

    route was going to json page not feed

--- controllers/api/petRoute.js
+++ controllers/api/petRoute.js
@@ -43,7 +43,7 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const newPetPost = await Pet.create(req.body);
-    res.status(200).json(newPetPost);
+    res.redirect("/feed");
   } catch (err) {
     res.status(400).json(err);
   }
